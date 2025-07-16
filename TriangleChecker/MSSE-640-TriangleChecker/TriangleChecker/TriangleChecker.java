c63ee08a3074c7832c568667c6084d930e1714a4
boozie4 <90736536+boozie4@users.noreply.github.com>
2025-07-16 14:24:37 -0500

    Add files via upload
    Second Commit for updated Java TriangleChecker. Added in code to handle input of 0 for triangle sides.

--- TriangleChecker/MSSE-640-TriangleChecker/TriangleChecker/TriangleChecker.java
+++ TriangleChecker/MSSE-640-TriangleChecker/TriangleChecker/TriangleChecker.java
@@ -5,22 +5,10 @@ public class TriangleChecker {
         int a, b, c;
         System.out.println("Enter the lengths of the first side of the triangle:");
         a=sc.nextInt();
-        if (a <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
         System.out.println("Enter the lengths of the second side of the triangle:");
         b=sc.nextInt();
-        if (b <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
         System.out.println("Enter the lengths of the third side of the triangle:");
         c=sc.nextInt();
-        if (c <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
         
         if (a==b && b==c) {
             System.out.println("The triangle is equilateral.");
