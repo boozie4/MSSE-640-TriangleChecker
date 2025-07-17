6a8beaf7c6e9cbad34e103f75d7fa12d4b5145d9
boozie4 <90736536+boozie4@users.noreply.github.com>
2025-07-16 21:30:01 -0500

    modified:   TriangleChecker/TriangleChecker.java     Seprated into different classes for better organization.

--- TriangleChecker/TriangleChecker.java
+++ TriangleChecker/TriangleChecker.java
@@ -1,33 +1,36 @@
 import java.util.Scanner;
 public class TriangleChecker {
-    public static void main(integers[] args) {
-        Scanner sc=new Scanner (System.in);
-        int a, b, c;
-        System.out.println("Enter the lengths of the first side of the triangle:");
-        a=sc.nextInt();
-        if (a <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
-        System.out.println("Enter the lengths of the second side of the triangle:");
-        b=sc.nextInt();
-        if (b <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
-        System.out.println("Enter the lengths of the third side of the triangle:");
-        c=sc.nextInt();
-        if (c <= 0) {
-            System.out.println("Invalid input. Side lengths must be positive integers.");
-            return;
-        }
-        
-        if (a==b && b==c) {
-            System.out.println("The triangle is equilateral.");
-        } else if (a==b || b==c || a==c) {
-            System.out.println("The triangle is isosceles.");
+    private int a, b, c;
+    private boolean isValidTriangle() {
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }   
+    private String triangleType() {
+        if (a == b && b == c) {
+            return "Equilateral";
+        } else if (a == b || b == c || a == c) {
+            return "Isosceles";
         } else {
-            System.out.println("The triangle is scalene.");
+            return "Scalene";
         }
     }
+    public void checkTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter side a: ");
+        a = scanner.nextInt();
+        System.out.print("Enter side b: ");
+        b = scanner.nextInt();
+        System.out.print("Enter side c: ");
+        c = scanner.nextInt();  
+        if (isValidTriangle()) {
+            System.out.println("The triangle is " + triangleType() + ".");
+        } else {
+            System.out.println("The sides do not form a valid triangle.");
+        }
+        scanner.close();
+    }
+    public static void main(String[] args) {
+        TriangleChecker triangleChecker = new TriangleChecker();
+        triangleChecker.checkTriangle();
+    }
 }
+// This code defines a TriangleChecker class that checks if three sides can form a triangle and determines
